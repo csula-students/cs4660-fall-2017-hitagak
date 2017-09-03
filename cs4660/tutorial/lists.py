c9b2fd14c3bebd77e6bf35269fb9a27c30d317e4
Hiroko Itagaki <hitagak@calstatela.edu>
2017-09-02 17:12:15 -0700

    test

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,29 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
-    pass
+    return li[0]
+
 
 def get_last_item(li):
     """Return the last item from the list"""
-   return li[-1]
+    return li[-1]
+
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    return (li[1], li[2])
+    return li[1:3]
+
 
 def get_sum(li):
     """Return the sum of the list items"""
-    return sum(li)
+    sum = 0
+    for num in li:
+        sum += num
+    return sum
+
 
 def get_avg(li):
     """Returns the average of the list items"""
-    pass sum(li)/len(li)
+    sum = float(get_sum(li))
+
+    return sum / len(li)
