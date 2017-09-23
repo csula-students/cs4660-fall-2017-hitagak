e0df939d1e6396dd5edfc4acb9f663faec5aabb3
Hiroko Itagaki <hitagak@calstatela.edu>
2017-09-22 23:32:51 -0700

    finished homework1

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -8,46 +8,31 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
-        with open(file_path) as f:
-            for line in f:
-                self.numbers.append(float(i) for i in line.split())
-
-
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        sum = 0
-        count = 0
-        for i in self.numbers[line_number]:
-            sum += i
-            count +=1
-        
-        return sum / count
-
+        pass
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        return max(self.numbers[line_number])
-
+        pass
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        return min(self.numbers[line_number])
-
-
+        pass
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        return sum(self.numbers[line_number])
+        pass
