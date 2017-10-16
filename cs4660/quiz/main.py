a337f4c18cd4024222c311ce091666076da63dff
Hiroko Itagaki <hitagak@calstatela.edu>
2017-10-15 18:28:07 -0700

    Finish Quiz2

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -12,10 +12,7 @@ TODO: implement Dijkstra utilizing the path with highest effect number
 
 import json
 import codecs
-try:
-    import Queue as Q
-except ImportError:
-    import queue as Q
+
 
 
 # http lib import for Python 2 and 3: alternative 4
@@ -57,17 +54,131 @@ def __json_request(target_url, body):
     response = json.load(reader(urlopen(req, jsondataasbytes)))
     return response
 
-def bfs (initial_node, dest_node):
 
 
+# TODO: implement BFS
+def bfs(initial_node, dest_node):
 
+    q = [initial_node]
+    # dictionary to store parents which was visited
+    parents = {}
+    # list to store nodes which was visited
+    visited_node = [initial_node['id']]  
+    # dictionary to store edge
+    edges = {}
+    # path to destination
+    path = []
+
+    while q:
+        node = q.pop(0)
+        for c in node['neighbors']:
+            child = get_state(c['id'])
+            edge = transition_state(node['id'], child['id'])
+
+            if child['id'] not in visited_node:
+                visited_node.append(child['id'])
+                parents[child['id']] = node['id']
+                edges[child['id']] = edge
+                q.append(child)
+
+            elif child['id'] == dest_node['id']:
+                visited_node.append(child['id'])
+                parents[child['id']] = node['id']
+                edges[child['id']] = edge
+                
+                while dest_node['id'] in parents:
+                    path.append(edges[dest_node['id']])
+                    dest_node['id'] = parents[dest_node['id']]
+                    path.reverse()
+
+                print_path(path, initial_node['id'])
+                
+            
+            
+
+
+# TODO: implement Dijkstra utilizing the path with highest effect number
+def dijkstra_search(initial_node, dest_node):
+
+  
+    q = []
+    q.append((0, initial_node))
+  
+    # dictionary to store parents which was visited
+    parents = {}
+    # list to store nodes which was visited
+    visited_node = []
+    # distionary to store weight
+    distance = {}
+    distance[initial_node] = 0
+    # dictionary to store edge
+    edges = {}
+    # path to destination
+    path = []
+
+    while q:
+        node =get_state(q.pop()[1])
+        visited_node.append(node['id'])
+
+        # check all neighbors nodes
+        for child in node['neighbors']:
+            edge = transition_state(node['id'], child['id'])
+            weight = distance[node['id']] + edge['event']['effect']
+     
+            if(child['id'] not in distance or weight > distance[child['id']]) and child['id'] not in visited_node:
+                
+                if child['id'] in distance:
+                    q.remove((distance[child['id']], child['id']))
+                
+                distance[child['id']] = weight
+                parents[child['id']] = node['id']
+                edges[child['id']] = edge
+                q.append((weight, child['id']))
+        q = sorted(q, key=lambda x:x[0])
+
+    
+    while dest_node in parents:
+        path.append(edges[dest_node])
+        dest_node = parents[dest_node]
+    path.reverse()
+
+    print_path(path, initial_node)
+
+
+# print path
+def print_path(pathes, initial_node):
+    previous_id = initial_node
+    total = 0
+    for path in pathes:
+        previous_node = get_state(previous_id)
+        next_id = path['id']
+        total += path['event']['effect']
+
+        print(previous_node['location']['name']+' (' + previous_id + ') : '+ path['action'] + ' ('+path['id']+') : '+ str(path['event']['effect']))
+        previous_id = next_id
+    print('Total hp : ' + str(total))
+
+
+# main code
 if __name__ == "__main__":
     # Your code starts here
-    empty_room = get_state('7f3dc077574c013d98b2de8f735058b4')
-    dark_room = get_state('f1f131f647621a4be7c71292e79613f9')
-   # print(empty_room)
-   # print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
+    empty = '7f3dc077574c013d98b2de8f735058b4'
+    dark =  'f1f131f647621a4be7c71292e79613f9'
+
+    empty_room = get_state(empty)
+    dark_room = get_state(dark)
+   
+   
+    print('BFS Path:')
+    bfs(empty_room,dark_room)
+    print('\n')
+    print('Dijkstra Path:')
+    dijkstra_search(empty,dark)
+
+    #print(empty_room)
+    #print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
     
+   
 
 
 
